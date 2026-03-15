8790c9a6fd02dbb43b5f8efb0a63d1533db4d89a
Manuel Rodriguez <valle.mrv@gmail.com>
2026-03-15 23:51:04 +0100

    multinodo

--- src/features/file-mentions/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * File mentions feature - exports
- */
-
-export * from "./file-mentions.types.js";
-export * from "./file-mentions.parser.js";
-export * from "./file-mentions.service.js";
-export * from "./file-mentions.ui.js";
